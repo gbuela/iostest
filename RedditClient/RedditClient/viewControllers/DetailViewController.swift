20fd9f58945f9c7cf15c9b9b671afa0d16fd3453
German Buela <german.buela@despegar.com>
2019-08-31 12:47:21 -0300

    storyboard and basic vcs

---
+++ RedditClient/RedditClient/viewControllers/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  RedditClient
+//
+//  Created by German Buela on 31/08/2019.
+//  Copyright © 2019 German Buela. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
---
+++ RedditClient/RedditClient/viewControllers/MasterViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MasterViewController.swift
+//  RedditClient
+//
+//  Created by German Buela on 31/08/2019.
+//  Copyright © 2019 German Buela. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController, UISplitViewControllerDelegate {
+    
+    @IBOutlet private weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Reddit Posts"
+        splitViewController?.preferredDisplayMode = .allVisible
+        splitViewController?.delegate = self
+    }
+
+
+    @IBAction private func dismissAllTapped() {
+        print("dismissAllTapped") // FIXME: implement
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailVC = segue.destination as? DetailViewController else {
+            return
+        }
+        print("showing detail") // FIXME: implement
+    }
+}
+
+extension MasterViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1 // FIXME: implement
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // FIXME: implement
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test cell"
+        return cell
+    }
+}
+
+// TODO: not performing segue automatically from storyboard connection
+// find out why and remove this
+extension MasterViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: tableView)
+    }
+}
+
